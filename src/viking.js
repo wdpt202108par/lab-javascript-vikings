3600f836f51630b42561849c931a2ededd5f6089
Cindy Confiant <cindy.confiant@gmail.com>
2021-09-16 20:11:21 +0200

    bonus 4 first part

--- src/viking.js
+++ src/viking.js
@@ -48,7 +48,15 @@ class Saxon extends Soldier {
 }
 
 // War
-class War {}
+class War {
+  constructor () {
+    this.vikingArmy=[];
+    this.saxonArmy=[];
+  }
+  addViking(Viking) {this.vikingArmy.push(Viking)}
+  addSaxon(Saxon) {this.saxonArmy.push(Saxon)}
+}
+
 
 
 
