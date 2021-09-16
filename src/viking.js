d07918ed6b581aa864b6ffeb1582f43c7b79765e
Silas Paul <silas.paul@outlook.fr>
2021-09-16 19:57:40 +0200

    iteration 1

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,24 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health = this.health - damage;
+  }
+}
+
+//const soldat1 = new Soldier(100, 20) // {pv: 100 , dps: 20}
+//soldat1.attack() // 20
 
 // Viking
-class Viking {}
+class Viking {
+  
+}
 
 // Saxon
 class Saxon {}
